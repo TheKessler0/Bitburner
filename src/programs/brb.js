c22c3c2d1f4bccf7963daf7890960ebd35fe2b01
TheKessler0 <lfritzsche1990@gmail.com>
2022-02-13 02:39:06 +0100

    edited brb.js

--- src/programs/brb.js
+++ src/programs/brb.js
@@ -1,75 +1,57 @@
 /** @param {import("C:/Users/lfrit/Desktop/Birburner/Bitburner/NetscriptDefinitions").NS } ns */
+
 export async function main(ns) {
+
 	ns.tail()
-	ns.disableLog('ALL');
-	ns.enableLog('print');
-	ns.clearLog();
+	ns.disableLog('ALL')
 
-	let time_between_frames = ((ns.args[0] * 60 * 1000) / 22)
-	let picture = '\n\n\n\n\n         a,  8a\n         `8, `8)                            ,adPPRg,\n          8)  ]8                        ,ad888888888b\n         ,8´,8´                    ,gPPR888888888888\n        ,8´, 8´                 ,ad8""   `Y888888888P\n        8)  8)              ,ad8""        (8888888""\n        8,  8,          ,ad8""            d888""\n        `8, `8,     ,ad8""            ,ad8""\n         `8, `" ,ad8""            ,ad8""\n            ,gPPR8b           ,ad8""\n           dP:::::Yb      ,ad8""\n           8):::::(8  ,ad8""\n           Yb:;;;:d888""\n            "8ggg8P"\n        \n        \n';
-	let offset = '		       TAKING A BREAK\n\n		  '
+	const flags = ns.flags([
+        ['minutes', 0.1]
+    ]);
 
-	await draw(ns, picture, offset, time_between_frames, ' [                    ]\n\n')
-	await ns.sleep(5000)
-	await draw(ns, picture, offset, time_between_frames, ' [■                   ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■                  ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■                 ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■                ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■               ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■              ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■             ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■            ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■           ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■          ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■         ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■        ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■       ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■      ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■     ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■■    ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■■■   ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■■■■  ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■■■■■ ]\n\n')
-	await draw(ns, picture, offset, time_between_frames, ' [■■■■■■■■■■■■■■■■■■■■]\n\n')
+	const divisions = 60
+	const delay = (flags.minutes * 60 * 1000) / divisions;
 
-	await draw2(ns, picture, offset, ' [■   ■■■■■■■■■■■■■■■■]\n\n')
-	await draw2(ns, picture, offset, ' [■■      ■■■■■■■■■■■■]\n\n')
-	await draw2(ns, picture, offset, ' [■■■         ■■■■■■■■]\n\n')
-	await draw2(ns, picture, offset, ' [■■■■            ■■■■]\n\n')
-	await draw2(ns, picture, offset, ' [■■■■■               ]\n\n')
+	let loadingbar = []
 
-	while (true) {
-		await draw2(ns, picture, offset, ' [■■■■■               ]\n\n')
-		await draw2(ns, picture, offset, ' [ ■■■■■              ]\n\n')
-		await draw2(ns, picture, offset, ' [  ■■■■■             ]\n\n')
-		await draw2(ns, picture, offset, ' [   ■■■■■            ]\n\n')
-		await draw2(ns, picture, offset, ' [    ■■■■■           ]\n\n')
-		await draw2(ns, picture, offset, ' [     ■■■■■          ]\n\n')
-		await draw2(ns, picture, offset, ' [      ■■■■■         ]\n\n')
-		await draw2(ns, picture, offset, ' [       ■■■■■        ]\n\n')
-		await draw2(ns, picture, offset, ' [        ■■■■■       ]\n\n')
-		await draw2(ns, picture, offset, ' [         ■■■■■      ]\n\n')
-		await draw2(ns, picture, offset, ' [          ■■■■■     ]\n\n')
-		await draw2(ns, picture, offset, ' [           ■■■■■    ]\n\n')
-		await draw2(ns, picture, offset, ' [            ■■■■■   ]\n\n')
-		await draw2(ns, picture, offset, ' [             ■■■■■  ]\n\n')
-		await draw2(ns, picture, offset, ' [              ■■■■■ ]\n\n')
-		await draw2(ns, picture, offset, ' [               ■■■■■]\n\n')
-		await draw2(ns, picture, offset, ' [■               ■■■■]\n\n')
-		await draw2(ns, picture, offset, ' [■■               ■■■]\n\n')
-		await draw2(ns, picture, offset, ' [■■■               ■■]\n\n')
-		await draw2(ns, picture, offset, ' [■■■■               ■]\n\n')
+	for (let i = 0; i < divisions; i++) {
+
+		loadingbar.push('[')
+		loadingbar[i] = loadingbar[i].padEnd(i + 1, '|')
+		loadingbar[i] = loadingbar[i].padEnd(divisions, '-') + ']'
+		loadingbar[i] = 'I´ll be back in aprox ' + flags.minutes + ' minutes!\n' + loadingbar[i]
+
 	}
-}
 
-async function draw(ns, picture, offset, time_between_frames, progressbar) {
-	await ns.clearLog()
-	await ns.print(picture + offset + progressbar)
-	await ns.sleep(time_between_frames)
-}
+	for (let i = 0; i < loadingbar.length; i++) {
 
-async function draw2(ns, picture, offset, progressbar) {
-	await ns.clearLog()
-	await ns.print(picture + offset + progressbar)
-	await ns.sleep(1000)
+		ns.clearLog()
+		ns.print(loadingbar[i])
+		await ns.sleep(delay)
+
+	}
+
+	loadingbar = []
+
+	for (let i = 0; i < divisions; i++) {
+
+		loadingbar.push('[')
+		loadingbar[i] = loadingbar[i].padEnd(i, '|') + '.'
+		loadingbar[i] = loadingbar[i].padEnd(divisions, '|') + ']'
+		loadingbar[i] = 'I´ll be back in aprox ' + flags.minutes + ' minutes!\n' + loadingbar[i]
+
+	}
+
+	for (let i = 0; i < loadingbar.length; i++) {
+
+		ns.clearLog()
+		ns.print(loadingbar[i])
+		await ns.sleep(1000)
+
+		if ( i == loadingbar.length - 1) {
+			i = 0
+		}
+
+	}
+
 }
