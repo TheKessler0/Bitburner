8298703898cb9b39867b6fa35b9035fb0c3a9067
TheKessler0 <lfritzsche1990@gmail.com>
2022-02-15 23:23:45 +0100

    re-wrote servinf.ts

---
+++ src/programs/tools/servinf.ts
@@ -0,0 +1,84 @@
+import { NS } from '@ns';
+
+export async function main(ns : NS) : Promise<void> {
+
+    ns.disableLog('ALL')
+
+    const HSERVnames = /**Hacknet-Server names*/ ['H01','H02','H03','H04','H05','H06','H07','H08','H09','H10','H11','H12','H13','H14','H15','H16','H17','H18','H19','H20']
+    const PSERVnames = /**Player-Server names */ ['S01','S02','S03','S04','S05','S06','S07','S08','S09','S10','S11','S12','S13','S14','S15','S16','S17','S18','S19','S20','S21','S22','S23','S24','S25']
+
+    while (true) {
+
+        ns.clearLog()
+
+        let prnt = '\n'
+
+        prnt += getPRNTsimple(HSERVnames)
+        prnt += '\n'
+        prnt += getPRNTsimple(PSERVnames)
+        prnt += ('\n').padStart(29,'=')
+        prnt += getPRNTcomplex(HSERVnames,PSERVnames)
+        prnt += padding('Home', formatSize(ns.getServerMaxRam('home')),Math.ceil((ns.getServerUsedRam('home') / ns.getServerMaxRam('home')) * 100) + ' %')
+
+        ns.print(prnt)
+
+        await ns.sleep(1000)
+    }
+
+    function getPRNTsimple (names) {
+
+        let result = ''
+
+        for (let i = 0; i < names.length; i++) {
+            result += padding(names[i],getsize(names[i]),getused(names[i]))
+        }
+
+        return result
+
+    }
+
+    function padding (name, size, utilization) {
+        return (name).padEnd(6,' ') + (size).padStart(12,' ') + (utilization).padStart(10,' ') + '\n'
+    }
+
+    function getsize (name) {
+        if (!ns.serverExists(name)) { return ('------ GB') }
+
+        return formatSize(ns.getServerMaxRam(name))
+    }
+
+    function formatSize (rawGB) {
+
+        if (rawGB >= 2**30) { return ((rawGB / (2**30)).toFixed(2) + ' EB') }
+        if (rawGB >= 2**20) { return ((rawGB / (2**20)).toFixed(2) + ' PB') }
+        if (rawGB >= 2**10) { return ((rawGB / (2**10)).toFixed(2) + ' TB') }
+
+        return ((rawGB).toFixed(2) + ' GB')
+    }
+
+    function getused (name) {
+
+        if (!ns.serverExists(name)) { return ('--- %') }
+
+        return (Math.ceil((ns.getServerUsedRam(name) / ns.getServerMaxRam(name)) * 100) + ' %')
+    }
+
+    function getPRNTcomplex (array1, array2) {
+        const names = [...array1, ...array2]
+        let size = 0
+        let used = 0
+
+        for (let i = 0; i < names.length; i++) {
+            if (ns.serverExists(names[i])) {
+                size += ns.getServerMaxRam(names[i])
+                used += ns.getServerUsedRam(names[i])
+            }
+        }
+        used = Math.ceil((used / size) * 100)
+        if (used == Infinity) { used = 0 }
+
+        return padding('ALL',formatSize(size), Math.ceil(used) + ' %')
+
+    }
+
+}
