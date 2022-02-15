b98d4e71a8842132ab8702a8f5557678c5a825a5
TheKessler0 <lfritzsche1990@gmail.com>
2022-02-15 03:01:20 +0100

    goodnight github

--- src/programs/xp-farmer.js
+++ src/programs/xp-farmer.js
@@ -1,4 +1,4 @@
-/** @param {import("C:/Users/lfrit/Desktop/Birburner/Bitburner/NetscriptDefinitions").NS } ns */
+/** @param {import("C:/Users/lfrit/Desktop/Bitburner/Bitburner/NetscriptDefinitions").NS } ns */
 export async function main(ns) {
     ns.tail()
 
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export async function main(ns : NS) : Promise<void> {
+    //
+}
