9b367288dac56c2ca88abf1b61ed0c3732d969b0
TheKessler0 <lfritzsche1990@gmail.com>
2022-03-23 03:27:35 +0100

    deleted now uneccessary files

--- src/programs/dependencies/1hack.ts
+++
@@ -1,7 +0,0 @@
-import { NS } from '@ns';
-
-export async function main(ns : NS) : Promise<void> {
-    const target:string = <string>ns.args[0]
-    await ns.sleep(ns.getHackTime(target) * 3);
-    await ns.hack(target);
-}
--- src/programs/dependencies/1weaken.ts
+++
@@ -1,7 +0,0 @@
-import { NS } from '@ns';
-
-export async function main(ns : NS) : Promise<void> {
-    const target:string = <string>ns.args[0]
-    await ns.sleep(0);
-    await ns.weaken(target);
-}
